23eb4e1d96e1dff5b1e91dad52bc399ade4d4f11
Jon Huffaker <jonathanhuffaker@gmail.com>
2015-08-08 10:51:57 -0500

    starting on JS

---
+++ main.js
@@ -0,0 +1,10 @@
+//below is just a test to ensure that there was no issue linking to html file
+//alert("test");
+
+function winnerTest {
+var winner =document.getElementbyId("cell1").value;
+ if(winner = "x" || "o")	{
+ 	alert("You win!");
+ }
+}
+
