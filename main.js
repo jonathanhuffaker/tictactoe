0fbea70c392467109c8b0e01389d118d6d005431
Mohamed Sharif <mohame@gmail.com>
2015-08-09 22:18:08 -0500

    trying to get x in the box

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
-//below is just a test to ensure that there was no issue linking to html file
-//alert("test");
+// below is just a test to ensure that there was no issue linking to html file
+// alert("test");
 
-function winnerTest {
-var winner =document.getElementbyId("cell1").value;
- if(winner = "x" || "o")	{
- 	alert("You win!");
- }
+// function winnerTest {
+// var winner =document.getElementbyId("cell1").value;
+//  if(winner = "x" || "o")	{
+//  	alert("You win!");
+//  }
+// }
+
+//I cant get the x to appear in the box.//
+
+
+function startGame(){
+	document.turn = "x"
+	
+	setMessage(document.turn + " gets to start");
+}
+
+function setMessage(msg){
+
+	document.getElementbyId("message").innerText = msg
+}
+
+function nextMove(Square){
+	square.innerText = document.turn;
 }
 
